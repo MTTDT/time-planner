22e7f4edde9f57085220805568af9249ed15c1e9
MTTDT <150801597+MTTDT@users.noreply.github.com>
2025-03-02 22:11:00 +0200

    newPage

---
+++ app/newPage/page.jsx
@@ -0,0 +1,12 @@
+"use client"
+import Image from "next/image";
+import DayCard from "./components/DayCard"
+import { Calendar } from "@heroui/react";
+
+export default function Home() {
+  return (
+    <div className="text-center">
+        <p>New Page</p>
+  </div>
+);
+}

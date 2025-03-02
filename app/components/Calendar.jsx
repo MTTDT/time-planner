959314e8dfdada0a8ca17a4b8a249eb9ff62809a
MTTDT <150801597+MTTDT@users.noreply.github.com>
2025-03-02 21:53:08 +0200

    Some prompts

---
+++ app/components/Calendar.jsx
@@ -0,0 +1,12 @@
+import {Calendar} from "@heroui/react";
+import {parseDate} from "@internationalized/date";
+
+const Calendar = () =>{
+  return (
+    <div className="flex gap-x-4">
+      <Calendar aria-label="Date (No Selection)" />
+      <Calendar aria-label="Date (Uncontrolled)" defaultValue={parseDate("2020-02-03")} />
+    </div>
+  );
+}
+export default Calendar;
--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,17 @@
+"use client"
 import Image from "next/image";
 import DayCard from "./components/DayCard"
+import { Calendar } from "@heroui/react";
 
 export default function Home() {
   return (
-    <div className="grid grid-cols-6 gap-6">
-      {
-        Array.from({ length: 30 }, (_, i) => (
-          <DayCard key={i} day={i+1} />
-        ))
-      }
-    </div>
-  );
+    // <div className="grid grid-cols-6 gap-6">
+    //   {
+    //     Array.from({ length: 30 }, (_, i) => (
+    //       <DayCard key={i} day={i+1} />
+    //     ))
+    //   }
+    // </div>
+  <Calendar/>
+);
 }

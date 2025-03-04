3cbecb7dc21ec72c73c1b5497c1482277aa357c0
ByBazar <alhabul228@gmail.com>
2025-03-04 19:36:47 +0300

    Added DayView and HomeMenu

--- app/components/TopMenu.jsx
+++ app/components/TopMenu.jsx
@@ -4,25 +4,29 @@ import lg_sun from '../assets/bx-sun.png';
 import lg_moon from '../assets/bx-moon.png';
 import '../globals.css';
 
-const TopMenu = ({theme, setTheme}) => {
-
+const TopMenu = ({ theme, setTheme, onHomeClick }) => {
     const toggle_mode = () => {
         theme == 'light' ? setTheme('dark') : setTheme('light');
     }
+    
     return (
         <div className={`top-menu ${theme}`}>
             <ul>
-                <li><a href="#">Home</a></li>
+                <li><a href="#" onClick={(e) => {
+                    e.preventDefault();
+                    onHomeClick();
+                }}>Home</a></li>
                 <li><a href="#">About</a></li>
             </ul>
             
             <Image 
-            src={theme == 'light' ? lg_sun : lg_moon}
-            alt="Toggle theme"
-            className='toggle-icon'
-            onClick={()=>{toggle_mode()}}/>
-        
+                src={theme == 'light' ? lg_sun : lg_moon}
+                alt="Toggle theme"
+                className='toggle-icon'
+                onClick={toggle_mode}
+            />
         </div>
     );
 }
-export default TopMenu
+
+export default TopMenu;
--- app/page.jsx
+++ app/page.jsx
@@ -4,20 +4,41 @@ import React, { useState, useEffect } from "react";
 import DayCard from "./components/DayCard"
 import { Calendar } from "@heroui/react";
 import TopMenu from "./components/TopMenu";
+import HomeMenu from "./components/HomeMenu";
+import DayView from "./components/DayView";
 
 export default function Home() {
-
   const [theme, setTheme] = useState('light');
+  const [currentView, setCurrentView] = useState('home'); // 'home', 'day', 'week', 'month'
+
   useEffect(() => {
     document.documentElement.classList.remove('light', 'dark');
     document.documentElement.classList.add(theme);
   }, [theme]);
 
+  const renderView = () => {
+    switch(currentView) {
+      case 'home':
+        return <HomeMenu setView={setCurrentView} />;
+      case 'day':
+        return <DayView />;
+      case 'week':
+        return <div>Week View Coming Soon</div>;
+      case 'month':
+        return <Calendar />;
+      default:
+        return <HomeMenu setView={setCurrentView} />;
+    }
+  };
+
   return (
-    <div className={`container ${theme }`}>
-      <TopMenu theme={theme} setTheme={setTheme}/>
-      <Calendar/>
-      <DayCard/>
+    <div className={`container ${theme}`}>
+      <TopMenu 
+        theme={theme} 
+        setTheme={setTheme}
+        onHomeClick={() => setCurrentView('home')}
+      />
+      {renderView()}
     </div>
-);
+  );
 }
